d19754d3a513bc8ddf402599135f23454bc38750
doomy <dahyun@kakao.com>
2020-10-24 14:01:05 +0900

    style: change tag align

--- pages/components/NavBarComponent.tsx
+++ pages/components/NavBarComponent.tsx
@@ -1,5 +1,13 @@
+import BookMarkIcon from "public/img/nav-bar/bookmark.svg";
+import ExploreIcon from "public/img/nav-bar/explore.svg";
 import HomeIcon from "public/img/nav-bar/home.svg";
+import ListIcon from "public/img/nav-bar/list.svg";
 import LogoIcon from "public/img/nav-bar/logo.svg";
+import ProfileIcon from "public/img/nav-bar/logo.svg";
+import MsgIcon from "public/img/nav-bar/message.svg";
+import MoreIcon from "public/img/nav-bar/more.svg";
+import NotiIcon from "public/img/nav-bar/notification.svg";
+
 import React from "react";
 
 export default function NavBarComponent(): React.ReactElement {
@@ -7,6 +15,13 @@ export default function NavBarComponent(): React.ReactElement {
     <div>
       <LogoIcon />
       <HomeIcon />
+      <ExploreIcon />
+      <NotiIcon />
+      <MsgIcon />
+      <BookMarkIcon />
+      <ListIcon />
+      <ProfileIcon />
+      <MoreIcon />
     </div>
   );
 }
