fe3c3aae7c7fe9a5d36873e6f3a5d7cb696698ce
doomy <dahyun@kakao.com>
2020-10-24 18:22:47 +0900

    build: add stylelint config
    - changed svg imgs
    - changed favicon
    - added `scss` base config

--- pages/components/NavBarComponent.tsx
+++ pages/components/NavBarComponent.tsx
@@ -1,27 +1,70 @@
+import styles from "./NavBarComponent.module.scss";
 import BookMarkIcon from "public/img/nav-bar/bookmark.svg";
 import ExploreIcon from "public/img/nav-bar/explore.svg";
 import HomeIcon from "public/img/nav-bar/home.svg";
 import ListIcon from "public/img/nav-bar/list.svg";
 import LogoIcon from "public/img/nav-bar/logo.svg";
-import ProfileIcon from "public/img/nav-bar/logo.svg";
 import MsgIcon from "public/img/nav-bar/message.svg";
 import MoreIcon from "public/img/nav-bar/more.svg";
 import NotiIcon from "public/img/nav-bar/notification.svg";
-
+import ProfileIcon from "public/img/nav-bar/profile.svg";
 import React from "react";
 
 export default function NavBarComponent(): React.ReactElement {
   return (
     <div>
-      <LogoIcon />
-      <HomeIcon />
-      <ExploreIcon />
-      <NotiIcon />
-      <MsgIcon />
-      <BookMarkIcon />
-      <ListIcon />
-      <ProfileIcon />
-      <MoreIcon />
+      <LogoIcon className={styles.logo} />
+      <nav>
+        <a>
+          <div className={styles.contentsWrapper}>
+            <HomeIcon />홈
+          </div>
+        </a>
+
+        <a>
+          <div className={styles.contentsWrapper}>
+            <ExploreIcon />
+            탐색하기
+          </div>
+        </a>
+        <a>
+          <div className={styles.contentsWrapper}>
+            <NotiIcon />
+            알림
+          </div>
+        </a>
+        <a>
+          <div className={styles.contentsWrapper}>
+            <MsgIcon />
+            쪽지
+          </div>
+        </a>
+        <a>
+          <div className={styles.contentsWrapper}>
+            <BookMarkIcon />
+            북마크
+          </div>
+        </a>
+        <a>
+          <div className={styles.contentsWrapper}>
+            <ListIcon />
+            리스트
+          </div>
+        </a>
+        <a>
+          <div className={styles.contentsWrapper}>
+            <ProfileIcon />
+            프로필
+          </div>
+        </a>
+        <a>
+          <div className={styles.contentsWrapper}>
+            <MoreIcon />
+            더보기
+          </div>
+        </a>
+      </nav>
+      <div>트윗</div>
     </div>
   );
 }
